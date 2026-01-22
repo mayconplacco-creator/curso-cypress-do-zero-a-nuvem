88e6b91d11397a113744a5736d177065147969e1
Maycon Velludo Placco <maycon.placco@scanbrazilconsulting.com>
2026-01-22 16:05:09 -0300

    Fix broken test and restore CI

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -4,9 +4,10 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.visit('./src/index.html')
   })
 
-  it('verifica o título da aplicação', () => {
-    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-  })
+ it('verifica o título da aplicação', () => {
+  cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+})
+
 
   it('preenche os campos obrigatórios e envia o formulário', () => {
 
